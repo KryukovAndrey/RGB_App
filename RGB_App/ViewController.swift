854cb1925bc23c4ab875555c6960e1a3249128a5
KryukovAndrey <kryukov.ios@yandex.ru>
2020-05-23 15:05:01 +0300

    App is working !)

--- RGB_App/ViewController.swift
+++ RGB_App/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rgbView: UIView!
+    @IBOutlet weak var sliderRedOutlet: UISlider!
+    @IBOutlet weak var sliderGreenOutlet: UISlider!
+    @IBOutlet weak var sliderBlueOutlet: UISlider!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sliderRed(_ sender: UISlider) {
+        rgbViewColor()
+        redLabel.text = String(roundf (sliderRedOutlet.value * 100) / 100)
+    }
+    @IBAction func sliderGreen(_ sender: UISlider) {
+        rgbViewColor()
+        greenLabel.text = String(roundf (sliderGreenOutlet.value * 100) / 100)
+    }
+    @IBAction func sliderBlue(_ sender: UISlider) {
+        rgbViewColor()
+        blueLabel.text = String(roundf (sliderBlueOutlet.value * 100) / 100)
+    }
+    
+    func rgbViewColor(){
+        rgbView.backgroundColor = UIColor(
+            red: CGFloat(sliderRedOutlet.value),
+            green: CGFloat(sliderGreenOutlet.value),
+            blue: CGFloat(sliderBlueOutlet.value),
+            alpha: 1.0
+        )
+    }
 }
 
